4b5310fbf5aa4acf9f462d7b19c1d930ce9cfa19
rikakks <15hatoyar@gmail.com>
2018-03-22 14:50:25 -0700

    Added user inputs and also organized into classes for wave attributes.

--- fft_on_array_new.py
+++ fft_on_array_new.py
@@ -2,28 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fftpack
 
-pi = 3.14159265359  #using np.pi causes floating point accuracy error, so we will use a float
-sin = np.sin
-frequency = [
-    5,
-    0.5,
-    1.0,
-]
-period = 50
-n_of_samples = 20000
+pi = 3.1415926535897932
+distance_earth_sun = 149.6 * (10**9)
+light_v = 3 * 10**8
+frequency = []
+while True:
+    frequency_input = input("Enter Frequency, otherwise hit return: ")
+    if frequency_input == '':
+        break
+    try:
+        frequency.append(float(frequency_input))
+    except ValueError:
+        print("Please enter a valid float.")
+while True:
+    try:
+        period = int(input("Enter period: "))
+        break
+    except ValueError:
+        print("Please enter a valid integer.")
+while True:
+    try:
+        n_of_samples = int(input("Enter number of samples: "))
+        break
+    except ValueError:
+        print("Please enter a valid integer.")
+observer_v = 2.0
+source_v = 0
 step_size = period / n_of_samples
-t = []
-
-for multiple in range(n_of_samples):
-    t.append(multiple * step_size)
+t = [multiple * step_size for multiple in range(n_of_samples)]
 
 
 class Wave:
     def __init__(self):
         pass
 
-    def frequency(self, n):
-        self.frequency = frequency[n]
+    def originalfrequency(self, n):
+        self.originalfrequency = n
 
     def period(self):
         self.period = period
@@ -32,83 +46,63 @@ class Wave:
         self.amplitude = 1 / len(frequency)
 
     def function(self):
-        self.function = []
-        for number in t:
-            self.function.append(
-                self.amplitude * sin(self.frequency * 2 * pi * number))
+        self.function = [
+            self.amplitude * np.sin(self.originalfrequency * 2.0 * pi * number)
+            for number in t
+        ]
+
+    def dopplerfrequency(self):
+        self.dopplerfrequency = (1 + (observer_v - source_v) /
+                                 (3 * 10**8)) * self.originalfrequency
+
+    def dopplerfunction(self):
+        self.dopplerfunction = [
+            self.amplitude * np.sin(self.dopplerfrequency * number)
+            for number in t
+        ]
 
 
-waves = {}
-a = 0
-wave_sum = []
-while a < len(frequency):
-    wave_a = "wave_{}".format(a)
-    waves[wave_a] = Wave()
-    waves[wave_a].frequency(a)
-    waves[wave_a].period()
-    waves[wave_a].amplitude()
-    waves[wave_a].function()
-    wave_sum += waves[wave_a].function
-    a += 1
+def waves():
+    global originalwave_sum, dopplerwave_sum
+    originalwaves = {}
+    dopplerwaves = {}
+    originalwave_sum = [0] * n_of_samples
+    dopplerwave_sum = [0] * n_of_samples
+    for a, freq in enumerate(frequency):
+        wave = Wave()
+        wave.originalfrequency(freq)
+        wave.dopplerfrequency()
+        wave.period()
+        wave.amplitude()
+        wave.function()
+        wave.dopplerfunction()
+        originalwave_sum += np.asarray(wave.function)
+        dopplerwave_sum += np.asarray(wave.dopplerfunction)
+        originalwaves["wave_{}".format(a)] = wave
+        dopplerwaves["wave_{}".format(a)] = wave
+    return
 
-#plot
-yf = scipy.fftpack.fft(wave_sum)
-xf = np.linspace(0.0, 1.0 / (2.0 * step_size), n_of_samples / 2)
-plt.figure(3)
-plt.plot(xf, 2.0 / n_of_samples * np.abs(yf[:n_of_samples // 2]))
-plt.show()
 
-print(aaaaa)
-plt.figure(1)
-plt.clf()
-plt.plot(t, f)
-plt.title(r'Sin function with frequency w = ' + str(w))
-plt.xlabel('time')
-plt.ylabel(r'sin(wt)')
-plt.show()
+def plot():
+    plt.figure("FFT Plot")
+    plt.title("FFT on Sum of Original Waves and Sum of Doppler Waves")
+    x = np.linspace(0.0, 1.0 / (2.0 * step_size), n_of_samples / 2)
+    y1 = scipy.fftpack.fft(originalwave_sum)
+    y2 = scipy.fftpack.fft(dopplerwave_sum)
+    plt.plot(
+        x,
+        2.0 / n_of_samples * np.abs(y1[:n_of_samples // 2]),
+        color="#11b9db",
+        label="Original")
+    plt.plot(
+        x,
+        2.0 / n_of_samples * np.abs(y2[:n_of_samples // 2]),
+        color="#093987",
+        label="Doppler")
+    plt.legend(('Original Wave Sum', 'Doppler Wave Sum'))
+    plt.show()
+    return
 
-################################
-################################
-################################
 
-# DOPPLER EFFECT
-
-# We asume it is a circular orbit with constant speed
-
-R = 149.6 * (10**9)  #earth-sun distance in meters
-
-c = 3 * (10**8)  #light speed in meters/seconds
-
-t = np.arange(0, T, 1)  #time array
-
-#np.savetxt(
-#    "dopplertime.csv", t, header="Doppler time array data: ", delimiter=",")
-
-v_earth = 2 * pi * R / T
-v_y_earth = v_earth * np.sin(2 * pi * t / T)
-print()
-print(v_earth)
-
-v_source = 0  #-275000 #velocity of the source in m/s
-w_wave = w
-
-# w_doppler = (1+(v_y_earth-v_source)/c)*w_wave
-w_doppler = (1 + (v_y_earth - v_source) / (3 * 10**8)) * w_wave
-print(max(w_doppler))
-f = np.sin(w_doppler * t)  #function array
-
-#np.savetxt(
-#    "dopplerfunction.csv",
-#    header="Doppler function array data: ",
-#    delimiter=",")
-
-plt.figure(2)
-plt.clf()
-plt.plot(t, f)
-plt.title(r'Sin function with doppler frequency')
-plt.xlabel('time')
-plt.ylabel(r'sin(w(t)t)')
-plt.show()
-
-print(len(f))
-print(len(t))
+waves()
+plot()
